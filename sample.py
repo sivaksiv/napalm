81b5bd71b4ef46d25038da74f1e873f5a584498d
Sivakumar Sukumar <sivaksiv@cisco.com>
2020-04-30 22:56:03 +0530

    test

--- sample.py
+++ sample.py
@@ -3,6 +3,13 @@ from napalm.base import get_network_driver
 driver=get_network_driver('nxos')
 dev = driver(hostname='10.197.174.68', username='admin',password="cisco!123")
 dev.open()
-bgp_neighbors = dev.cli(['show version'])
+bgp_neighbors = dev.cli(['show clock'])
+print(dev.get_facts())
 dev.close()
 print(json.dumps(bgp_neighbors, sort_keys=True, indent=4))
+
+#####	ADD CONFIG  #####
+with driver(hostname='10.197.174.68', username='admin',password="cisco!123") as device:
+  device.load_merge_candidate(filename='add_acl.cfg')
+  print(device.compare_config())
+  device.commit_config()
